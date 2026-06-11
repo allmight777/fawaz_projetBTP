ab62903292a2c19993b071ab59314100030ac2a7
allmight777 <agoliganange15@gmail.com>
2026-06-11 14:39:08 +0100

    11/06/2026

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-php artisan migrate --force
-php artisan db:seed --force
+php artisan migrate:fresh --seed --force
 php artisan config:cache
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
