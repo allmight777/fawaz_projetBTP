f59d1daf8f28a88d1499f40d66a522d272b80eb4
allmight777 <agoliganange15@gmail.com>
2026-09-24 18:16:07 +0100

    fix: remove destructive migrate:fresh from startup

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-php artisan migrate:fresh --seed --force
+php artisan migrate --force
+if [ "$RUN_SEED" = "true" ]; then php artisan db:seed --force; fi
 php artisan config:cache
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
